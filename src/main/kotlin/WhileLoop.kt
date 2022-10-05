75d9d7849cfeaf7677c844e65d8490c7b34bc314
Talo <talo90015@gmail.com>
2022-10-05 22:10:32 +0800

    while loop

---
+++ src/main/kotlin/WhileLoop.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var number = 0
+    var secondNumber = 10
+
+    while (number <= 10)
+        println(number++)
+    println("\n")
+    while (secondNumber >= 0)
+        println(secondNumber--)
+}
