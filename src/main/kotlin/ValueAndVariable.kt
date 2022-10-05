9af1d539ab818e763dfeefd07bc8c090f2b90901
Talo <talo90015@gmail.com>
2022-10-05 18:45:17 +0800

    Value And Variable

---
+++ src/main/kotlin/ValueAndVariable.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val valueInt = 1
+    val valueString = "ABC"
+//    valueString = "BCD"   val(value) 的值不能被改變
+
+    var variableInt = 10
+    var variableString = "DEF"
+    variableString = "ABC"  //var(variable) 的值內可被更改
+
+}
