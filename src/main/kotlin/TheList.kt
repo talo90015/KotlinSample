cf03e60f90180b1584bf51ee92049c67e5f6e9a1
Talo <talo90015@gmail.com>
2022-10-05 21:25:53 +0800

    List collection

---
+++ src/main/kotlin/TheList.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val list = listOf(0, 1, 2, 3, 4)
+    val isMutableList = mutableListOf(0, 1, 2, 3, 4)
+
+    println(list)
+    println(list::class.java.typeName)
+
+    println(isMutableList)
+    isMutableList.add(5)
+    println(isMutableList)
+    println(isMutableList::class.java.typeName)
+}
